badce8ef31c64a3539f104f0b1af0abd6f61bf18
Faris Chebib <faris@theluckybead.com>
2019-07-22 15:37:22 -0600

    Call individual benchmarks

--- app.py
+++ app.py
@@ -1,6 +1,22 @@
 # DB connection boilerplate
 
+import io
 import psycopg2
+import psycopg2.extras
+
+# parsers
+import datetime
+
+# metrics
+import time
+from functools import wraps
+from memory_profiler import memory_usage
+
+# data fetchers
+from typing import Iterator, Dict, Any, Optional
+from urllib.parse import urlencode
+import requests
+
 
 connection = psycopg2.connect(
     host="127.0.0.1",
@@ -41,7 +57,6 @@ def create_staging_table(cursor) -> None:
 
 
 # parsers
-import datetime
 
 def parse_first_brewed(text: str) -> datetime.date:
     """Return a datetime object of text, a string
@@ -52,16 +67,13 @@ def parse_first_brewed(text: str) -> datetime.date:
     """
     parts = text.split('/')
     if len(parts) == 2:
-        return datetime.date(int(parts[1], int(parts[0])), 1)
+        return datetime.date(int(parts[1]), int(parts[0]), 1)
     elif len(parts) == 1:
         return datetime.date(int(parts[0]), 1, 1)
     else:
         assert False, 'Unknown date format'
 
 # data fetchers
-from typing import Iterator, Dict, Any
-from urllib.parse import urlencode
-import requests
 
 def iter_beers_from_api(page_size: int = 5) -> Iterator[Dict[str, Any]]:
     session = requests.Session()
@@ -84,9 +96,6 @@ def iter_beers_from_api(page_size: int = 5) -> Iterator[Dict[str, Any]]:
         page += 1
 
 # metrics
-import time
-from functools import wraps
-from memory_profiler import memory_usage
 
 def profile(fn):
     @wraps(fn)
@@ -144,7 +153,7 @@ def insert_one_by_one(connection, beers: Iterator[Dict[str, Any]]) -> None:
 
 @profile
 def insert_executemany(connection, beers: Iterator[Dict[str, Any]]) -> None:
-    with connection.cursor as cursor:
+    with connection.cursor() as cursor:
         create_staging_table(cursor)
         all_beers = [{
             **beer,
@@ -237,9 +246,247 @@ def insert_execute_batch(connection, beers: Iterator[Dict[str, Any]]) -> None:
             );
         """, all_beers)
 
+@profile
+def insert_execute_batch_iterator(
+    connection,
+    beers: Iterator[Dict[str, Any]],
+    page_size: int = 100
+) -> None:
+    with connection.cursor() as cursor:
+        create_staging_table(cursor)
+
+        iter_beers = ({
+            **beer,
+            'first_brewed': parse_first_brewed(beer['first_brewed']),
+            'volume': beer['volume']['value'],
+        } for beer in beers)
+
+        psycopg2.extras.execute_batch(cursor, """
+            INSERT INTO staging_beers VALUES (
+                %(id)s,
+                %(name)s,
+                %(tagline)s,
+                %(first_brewed)s,
+                %(description)s,
+                %(image_url)s,
+                %(abv)s,
+                %(ibu)s,
+                %(target_fg)s,
+                %(target_og)s,
+                %(ebc)s,
+                %(srm)s,
+                %(ph)s,
+                %(attenuation_level)s,
+                %(brewers_tips)s,
+                %(contributed_by)s,
+                %(volume)s
+            );
+        """, iter_beers, page_size=page_size)
+
+
+@profile
+def insert_execute_values(connection, beers: Iterator[Dict[str, Any]]) -> None:
+    with connection.cursor() as cursor:
+        create_staging_table(cursor)
+        psycopg2.extras.execute_values(cursor, """
+            INSERT INTO staging_beers VALUES %s;
+        """, [(
+            beer['id'],
+            beer['name'],
+            beer['tagline'],
+            parse_first_brewed(beer['first_brewed']),
+            beer['description'],
+            beer['image_url'],
+            beer['abv'],
+            beer['ibu'],
+            beer['target_fg'],
+            beer['target_og'],
+            beer['ebc'],
+            beer['srm'],
+            beer['ph'],
+            beer['attenuation_level'],
+            beer['brewers_tips'],
+            beer['contributed_by'],
+            beer['volume']['value'],
+        ) for beer in beers])            
+
+
+@profile
+def insert_execute_values_iterator(connection, beers: Iterator[Dict[str, Any]]) -> None:
+    with connection.cursor() as cursor:
+        create_staging_table(cursor)
+        psycopg2.extras.execute_values(cursor, """
+            INSERT INTO staging_beers VALUES %s;
+        """, ((
+            beer['id'],
+            beer['name'],
+            beer['tagline'],
+            parse_first_brewed(beer['first_brewed']),
+            beer['description'],
+            beer['image_url'],
+            beer['abv'],
+            beer['ibu'],
+            beer['target_fg'],
+            beer['target_og'],
+            beer['ebc'],
+            beer['srm'],
+            beer['ph'],
+            beer['attenuation_level'],
+            beer['brewers_tips'],
+            beer['contributed_by'],
+            beer['volume']['value'],
+        ) for beer in beers))
+
+@profile
+def insert_execute_values_iterator(
+    connection,
+    beers: Iterator[Dict[str, Any]],
+    page_size: int = 100,
+) -> None:
+    with connection.cursor() as cursor:
+        create_staging_table(cursor)
+        psycopg2.extras.execute_values(cursor, """
+            INSERT INTO staging_beers VALUES %s;
+        """, ((
+            beer['id'],
+            beer['name'],
+            beer['tagline'],
+            parse_first_brewed(beer['first_brewed']),
+            beer['description'],
+            beer['image_url'],
+            beer['abv'],
+            beer['ibu'],
+            beer['target_fg'],
+            beer['target_og'],
+            beer['ebc'],
+            beer['srm'],
+            beer['ph'],
+            beer['attenuation_level'],
+            beer['brewers_tips'],
+            beer['contributed_by'],
+            beer['volume']['value'],
+        ) for beer in beers), page_size=page_size)
+
+
+# Using Copy
+def clean_csv_value(value: Optional[Any]) -> str:
+    if value is None:
+        return r'\N'
+    return str(value).replace('\n', '\\n')
+
+@profile
+def copy_stringio(connection, beers: Iterator[Dict[str, Any]]) -> None:
+    with connection.cursor() as cursor:
+        create_staging_table(cursor)
+        csv_file_like_object = io.StringIO()
+        for beer in beers:
+            csv_file_like_object.write('|'.join(map(clean_csv_value, (
+                beer['id'],
+                beer['name'],
+                beer['tagline'],
+                parse_first_brewed(beer['first_brewed']),
+                beer['description'],
+                beer['image_url'],
+                beer['abv'],
+                beer['ibu'],
+                beer['target_fg'],
+                beer['target_og'],
+                beer['ebc'],
+                beer['srm'],
+                beer['ph'],
+                beer['attenuation_level'],
+                beer['contributed_by'],
+                beer['brewers_tips'],
+                beer['volume']['value'],                
+            )))+ '\n')
+        csv_file_like_object.seek(0)
+        cursor.copy_from(csv_file_like_object, 'staging_beers', sep='|')
+
+
+class StringIteratorIO(io.TextIOBase):
+    def __init__(self, iter: Iterator[str]):
+        self._iter = iter
+        self._buff = ''
+    
+    def readable(self) -> bool:
+        return True
+    
+    def _read1(self, n: Optional[int] = None) -> str:
+        while not self._buff:
+            try:
+                self._buff = next(self._iter)
+            except StopIteration:
+                break
+        ret = self._buff[:n]
+        self._buff = self._buff[len(ret):]
+        return ret
+    
+    def read(self, n: Optional[int] = None) -> str:
+        line = []
+        if n is None or n < 0:
+            while True:
+                m = self._read1(n)
+                if not m:
+                    break
+                line.append(m)
+        else:
+            while n > 0:
+                m = self._read1(n)
+                if not m:
+                    break
+                n -= len(m)
+                line.append(m)
+        return ''.join(line)
+
+
+# Now use this iterator with COPY
+@profile
+def copy_string_iterator(
+    connection,
+    beers: Iterator[Dict[str, Any]],
+    size: int = 8192
+) -> None:
+    with connection.cursor() as cursor:
+        create_staging_table(cursor)
+
+        beer_gen = (
+            '|'.join(map(clean_csv_value, (
+                beer['id'],
+                beer['name'],
+                beer['tagline'],
+                parse_first_brewed(beer['first_brewed']).isoformat(),
+                beer['description'],
+                beer['image_url'],
+                beer['abv'],
+                beer['ibu'],
+                beer['target_fg'],
+                beer['target_og'],
+                beer['ebc'],
+                beer['srm'],
+                beer['ph'],
+                beer['attenuation_level'],
+                beer['brewers_tips'],
+                beer['contributed_by'],
+                beer['volume']['value'],                
+            ))) + '\n'
+            for beer in beers
+        )
+        beers_string_iterator = StringIteratorIO(beer_gen)
+        cursor.copy_from(beers_string_iterator, 'staging_beers', sep='|', size=size)
+
+
 def main() -> None:
-    beers = list(iter_beers_from_api) * 100
+    print('loading beers')
+    beers = list(iter_beers_from_api()) * 100
+    print(f'\n loaded {len(beers)} beers \n')
+    print('now for benchmarks')
+
     insert_one_by_one(connection, beers)
+    insert_executemany(connection, beers)
+    insert_execute_values_iterator(connection, beers, page_size=100)
+    insert_execute_values_iterator(connection, beers, page_size=1000)
+    copy_string_iterator(connection, beers, size=1024)
+    copy_string_iterator(connection, beers, size=8192)
 
 
 if __name__ == '__main__':
